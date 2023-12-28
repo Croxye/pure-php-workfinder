f752cf907c9aa3f88a71c4308faf1fb45c4617a3
NecipAkgz <nexco@hotmail.com>
2023-12-28 20:03:18 +0300

    Refactor routing and link paths for improved readability and maintainability
    Consolidate route definitions for better organization and simplify links
    for easier maintenance and future expansion. Update link paths across
    files to align with revised routing.

---
+++ controllers/listings/index.php
@@ -0,0 +1,4 @@
+<?php
+echo '<h1>Listings Page</h1>';
+
+?>
--- public/index.php
+++ public/index.php
@@ -2,6 +2,20 @@
 
 require '../helpers.php';
 
-require loadView('home');
+$routes = [
+  '/'                => 'controllers/home.php',
+  '/listings'        => 'controllers/listings/index.php',
+  '/listings/create' => 'controllers/listings/create.php',
+  '404'              => 'controllers/404.php'
+];
+
+$uri = $_SERVER['REQUEST_URI'];
+
+if (array_key_exists($uri, $routes)) {
+  require base_path($routes[$uri]);
+} else {
+  require base_path($routes['404']);
+}
+
 
 ?>
